d0e0e3e27b7d3863b8b5e8287b8b2276a8814be3
jzakrzewska <askazakrzewska@gmail.com>
2020-10-17 18:16:58 +0200

    co to jest za zmiana z komentarzem z pycharma

--- halohalo.py
+++ halohalo.py
@@ -1,4 +1,5 @@
 print("helou python")
 print("pierwsza zmiana")
 print("trzecia zmiana")
-print("zmiana z poziomu github"
+print("zmiana z poziomu github")
+print("zmiany jeszcze z poziomu pycharma")
